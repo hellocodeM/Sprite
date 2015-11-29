32b1d60adc9c8939c5391e1ce8720ece0ae6459b
HelloCodeMing <huanmingwong@163.com>
2015-11-29 23:46:06 +0800

    finish: hello world

---
+++ src/include/types.h
@@ -0,0 +1,20 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#ifndef NULL
+    #define NULL 0
+#endif
+
+#ifndef TRUE
+    #define TRUE 1
+    #define FALSE 0
+#endif
+
+typedef unsigned int uint32_t;
+typedef int int32_t;
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned char uint8_t;
+typedef char int8_t;
+
+#endif // TYPES_H
