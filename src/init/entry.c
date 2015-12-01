56a3eb53266bceb90bb4089c7a8f1e42a0f4830c
HelloCodeMing <huanmingwong@163.com>
2015-12-01 12:47:13 +0800

    finish: GDT

--- src/init/entry.c
+++ src/init/entry.c
@@ -1,10 +1,11 @@
 #include "printk.h"
 #include "debug.h"
+#include "gdt.h"
 
 int kern_entry() {
     init_debug();
+    init_gdt();
     console_clear();
     printk("%d %x %s\n", 123, 0x123F, "hello, code");
-    panic("test");
     return 0;
 }
